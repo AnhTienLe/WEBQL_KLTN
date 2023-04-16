bda733a65d4979bc1abde6c2272ea20596a469e9
Lê Trần Anh Tiến <100113292+AnhTienLe@users.noreply.github.com>
2023-04-16 12:11:43 +0700

    Add files via upload

---
+++ quantri_admin/gioithieu.php
@@ -0,0 +1,98 @@
+<div class="row">
+    <ol class="breadcrumb">
+        <li><a href="#"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
+        <li class="active"></li>
+    </ol>
+</div><!--/.row-->
+
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">Dashboard</h1>
+    </div>
+</div><!--/.row-->
+
+<div class="row">
+    <div class="col-xs-12 col-md-6 col-lg-3">
+        <div class="panel panel-blue panel-widget ">
+            <div class="row no-padding">
+                <div class="col-sm-3 col-lg-5 widget-left">
+                    <svg class="glyph stroked bag"><use xlink:href="#stroked-bag"></use></svg>
+                </div>
+                <div class="col-sm-9 col-lg-7 widget-right">
+                    <div class="large">100</div>
+                    <div class="text-muted">Đơn hàng</div>
+                </div>
+            </div>
+            <div class=""> <center><p>more infor </p> </center></div>
+        </div>
+       
+    </div>
+    <div class="col-xs-12 col-md-6 col-lg-3">
+        <div class="panel panel-orange panel-widget">
+            <div class="row no-padding">
+                <div class="col-sm-3 col-lg-5 widget-left">
+                    <svg class="glyph stroked empty-message"><use xlink:href="#stroked-empty-message"></use></svg>
+                </div>
+                <div class="col-sm-9 col-lg-7 widget-right">
+                    <div class="large">50</div>
+                    <div class="text-muted">Bình luận</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-12 col-md-6 col-lg-3">
+        <div class="panel panel-teal panel-widget">
+            <div class="row no-padding">
+                <div class="col-sm-3 col-lg-5 widget-left">
+                    <svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg>
+                </div>
+                <div class="col-sm-9 col-lg-7 widget-right">
+                    <div class="large">100</div>
+                    <div class="text-muted">Thành viên</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-12 col-md-6 col-lg-3">
+        <div class="panel panel-red panel-widget">
+            <div class="row no-padding">
+                <div class="col-sm-3 col-lg-5 widget-left">
+                    <svg class="glyph stroked app-window-with-content"><use xlink:href="#stroked-app-window-with-content"></use></svg>
+                </div>
+                <div class="col-sm-9 col-lg-7 widget-right">
+                    <div class="large">100.K</div>
+                    <div class="text-muted">Người theo dõi</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div><!--/.row-->
+
+<div class="row">
+    <div class="col-lg-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">Giới thiệu</div>
+            <div class="panel-body">
+                <div class="canvas-wrapper">
+                  <p>Book Store được thành lập vào ngày 26/05/20011
+                   Lầu 5, 387-389 Hai Bà Trưng Quận 3 TP HCM Công Ty Cổ Phần Phát Hành Sách TP HCM - FAHASA60 - 62 Lê Lợi, Quận 1, TP. HCM, Việt Nam </p>
+                   </br>
+                 
+                   Fahasa.com nhận đặt hàng trực tuyến và giao hàng tận nơi. KHÔNG hỗ trợ đặt mua và nhận hàng trực tiếp tại văn phòng cũng như tất cả Hệ Thống Fahasa trên toàn quốc.
+                    <br/>
+                    <p>Để xây dựng Thương hiệu mạnh, một trong những định hướng quan trọng hàng đầu của FAHASA là chiến lược phát triển nguồn nhân lực - mấu chốt của mọi sự thành công. FAHASA có hơn 2.200 CB-CNV, trình độ chuyên môn giỏi, nhiệt tình, năng động, chuyên nghiệp. Lực lượng quản lý FAHASA có thâm niên công tác, giỏi nghiệp vụ nhiều kinh nghiệm, có khả năng quản lý tốt và điều hành đơn vị hoạt động hiệu quả.</p>
+                    <br/>
+                   
+                    <p>Về hàng hóa
+
+Công ty FAHASA chuyên kinh doanh: sách quốc văn, ngoại văn, văn hóa phẩm, văn phòng phẩm, dụng cụ học tập, quà lưu niệm, đồ chơi dành cho trẻ em… Một số Nhà sách trực thuộc Công ty FAHASA còn kinh doanh các mặt hàng siêu thị như: hàng tiêu dùng, hàng gia dụng, hóa  mỹ phẩm…
+
+Sách quốc văn với nhiều thể loại đa dạng như sách giáo khoa – tham khảo, giáo trình, sách học ngữ, từ điển, sách tham khảo thuộc nhiều chuyên ngành phong phú: văn học, tâm lý – giáo dục, khoa học kỹ thuật, khoa học kinh tế - xã hội, khoa học thường thức, sách phong thủy, nghệ thuật sống, danh ngôn, sách thiếu nhi, truyện tranh, truyện đọc, từ điển, công nghệ thông tin, khoa học – kỹ thuật, nấu ăn, làm đẹp...  của nhiều Nhà xuất bản, nhà cung cấp sách có uy tín như: NXB Trẻ, Giáo Dục, Kim Đồng, Văn hóa -Văn Nghệ, Tổng hợp TP.HCM, Chính Trị Quốc Gia; Công ty Đông A, Nhã Nam, Bách Việt, Alphabook, Thái Hà, Minh Lâm, Đinh Tị, Minh Long, TGM, Sáng Tạo Trí Việt, Khang Việt, Toàn Phúc…</p>
+                    <br/>
+                    <p>
+                       Mang trong mình SỨ MỆNH <b> “MANG TRI THỨC, VĂN HÓA ĐỌC ĐẾN VỚI MỌI NHÀ”!</b></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div><!--/.row-->
